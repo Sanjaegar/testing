98d3148c65b89dac9ef4cb0cf20b41d3f9cff63f
sanjaegar <ariasukmacendikiawan1@gmail.com>
2023-10-08 12:28:24 +0700

    inisiasi base

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index() 
+    {
+echo "<h1>Perkenalkan</h1>";
+echo"Nama saya Aria Sukma Cendikiawan
+     Saya tinggal di daerah jakarta selatan
+     olahraga yang saya sukai adalah
+     Sepakbola dan basket";
+    }
+}
